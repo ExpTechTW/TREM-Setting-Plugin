a50590e85451ee27bf963f320a5feaafae27c011
YuYu1015 <44525760+whes1015@users.noreply.github.com>
2024-12-02 06:01:56 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-class Plugin {
-  #ctx;
-
-  constructor(ctx) {
-    this.#ctx = ctx;
-  }
-
-  onLoad() {
-    const settingButton = document.querySelector('#setting');
-	if(!settingButton) return;
-    settingButton.style.display = 'flex';
-    logger.info("Successfully show Setting Button!");
-  }
-}
-
-module.exports = Plugin;
+module.exports = function (ctx) {
+  ctx.on('load', () => {
+    document.getElementById('setting').style.display = 'flex';
+    logger.info('Successfully show Setting Button!');
+  });
+};
